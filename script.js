58acf79367581d5907ae370a7f7abf606b91530e
Atomic_Science <atomic1science@gmail.com>
2018-05-14 07:13:49 -0400

    Everything working now

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.getElementById("start").onclick = function () {
-	location.href = "activiti.html";
+timeInt = 60;
+money = 50;
+water = 100;
+setInterval(function(){ 
+	if(timeInt == 0) {
+		timeInt = 60;
+		money -= 25;
+	};
+	timeInt -= 1;
+	water -= 20;
+	document.getElementById("day").innerHTML = "Day End: " + timeInt.toString();
+	document.getElementById("water").innerHTML = "Water: " + water.toString() + "L/100L";
+	document.getElementById("money").innerHTML = "Money: " + money.toString() + "/50";
+}, 1000);
+function payTime () {
+	money += 1;
+};
+function waterGet () {
+	water = 110;
 };
